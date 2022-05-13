1ff63adff255e18e0d6e8c2b4013b22accc26dfe
Olesya <asa88.88@bk.ru>
2022-05-13 10:46:13 +0300

    fineshed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,89 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, harum!
+      Earum aut, dolore dicta vitae quas porro, alias rem optio numquam hic
+      reprehenderit quae similique debitis? Voluptate voluptatibus dolor fugit
+      blanditiis harum sit nulla dignissimos ut libero rem, laboriosam, cum sunt
+      ea qui esse quasi facilis praesentium accusamus maiores. Nostrum!
+    </p>
   </div>
 </template>
